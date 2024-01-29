5cc004c61cd84e40038fb1348931b9eae2e16ad6
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2024-01-29 17:19:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from utility import TestingModels
 
 if __name__ == '__main__':
     # Feature engineering (biosignals, video, early fusion, binary biosignals+video+ef)
-    # FeatureExtraction.extract_bio_features()
-    # FeatureExtraction.extract_video_features()
+    FeatureExtraction.extract_bio_features()
+    FeatureExtraction.extract_video_features()
 
     # Data generation
-    # DataGeneration.binarize_biosignals_and_video_features()
-    # DataGeneration.early_fusion()
-    # DataGeneration.binarize_early_fusion_features()
+    DataGeneration.binarize_biosignals_and_video_features()
+    DataGeneration.early_fusion()
+    DataGeneration.binarize_early_fusion_features()
 
     # Testing Models
     TestingModels.binary_classification()
-    # TestingModels.multiclass_classification()
+    TestingModels.multiclass_classification()
